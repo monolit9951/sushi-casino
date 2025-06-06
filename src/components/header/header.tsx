6959fa445f17b8f08beb43f658df4678607a5955
ivankalituk <63204841+ivankalituk@users.noreply.github.com>
2025-06-06 13:23:44 +0300

    header

---
+++ src/components/header/header.tsx
@@ -0,0 +1,17 @@
+import { FC } from "react";
+import './header.scss'
+import mainLogo from '../../assets/images/MainLogo.svg'
+const Header: FC = () => {
+    return(
+        <header>
+            <div className="header_container">
+                <img src={mainLogo} alt="Logo" />
+                <a className="header_link" href="#">
+                    <span className="header_link_text">Our website</span>
+                </a>
+            </div>
+        </header>
+    )
+}
+
+export default Header
