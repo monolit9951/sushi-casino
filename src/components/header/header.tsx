f4231c5bf4d2a2480e34e05f582375da7a86c5ff
ivankalituk <63204841+ivankalituk@users.noreply.github.com>
2025-06-06 19:39:39 +0300

    header logo link fix

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,11 +1,15 @@
 import { FC } from "react";
 import './header.scss'
 import mainLogo from '../../assets/images/MainLogo.svg'
+import { Link } from "react-router-dom";
 const Header: FC = () => {
     return(
         <header>
             <div className="header_container">
-                <img src={mainLogo} alt="Logo" />
+                <Link className="header_logo_link" to={'/'}>
+                    <img src={mainLogo} alt="Logo" />
+                </Link>
+
                 <a className="header_link" href="https://neptunessushi.com/">
                     <span className="header_link_text">Our website</span>
                 </a>
