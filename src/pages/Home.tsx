dedf258c451ceaa99108c5fae3567021b56bf46d
ivankalituk <63204841+ivankalituk@users.noreply.github.com>
2025-06-06 14:57:53 +0300

    roulete whithout items

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,7 @@
 export type Rarity = "COMMON" | "UNCOMMON" | "RARE" | "EPIC" | "LEGENDARY";
 import Header from "../components/header/header";
 import './home.scss'
+import Roulette from "../components/roulette/roulette";
 
 function Home() {
   // const { items /* isError, isLoading */ } = useItemsSet();
@@ -27,6 +28,7 @@ function Home() {
         <div className="main_description">
           <div className="main_description_first">Zakręć kołem i wygraj sushi!</div>
           <div className="main_description_second">Wpisz swój kod promocyjny, zakręć ruletką i zgarnij smakowite nagrody od naszej restauracji!</div>
+          <Roulette />
         </div>
       </div>
     </main>
