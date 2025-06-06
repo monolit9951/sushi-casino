e3b0971666be1c1edfb95f21754c4929ec649462
ivankalituk <63204841+ivankalituk@users.noreply.github.com>
2025-06-06 15:46:55 +0300

    Bottom item list

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,47 +2,7 @@ export type Rarity = "COMMON" | "UNCOMMON" | "RARE" | "EPIC" | "LEGENDARY";
 import Header from "../components/header/header";
 import './home.scss'
 import Roulette from "../components/roulette/roulette";
-import RouletteItem from "../components/rouletteItem/rouletteItem";
 import Item from "../components/item/item";
-
-function Home() {
-  // const { items /* isError, isLoading */ } = useItemsSet();
-  // const sortedItems = useMemo(
-  //   () => items?.sort((a, b) => b.probability - a.probability),
-  //   [items],
-  // );
-  // console.log(sortedItems);
-
-  return (
-    <main>
-      {/* <Logo src="/LogoCasiBLACK.svg" alt="Lucky Sushi Logo" />
-      <CaseOpener items={sortedItems || []} />
-      <SushiLink href="https://neptunessushi.com/" target="_blank">
-        Order more sushi
-      </SushiLink>
-      {sortedItems && <ListItems items={sortedItems} />}
-      <InfoButton>
-        <InfoModal />
-      </InfoButton> */}
-      <Header />
-
-      <div className="main_container">
-        <div className="main_description">
-          <div className="main_description_first">Zakręć kołem i wygraj sushi!</div>
-          <div className="main_description_second">Wpisz swój kod promocyjny, zakręć ruletką i zgarnij smakowite nagrody od naszej restauracji!</div>
-        </div>
-
-        <Roulette />
-
-        <div className="itemsList">
-          <div className="itemsList_heading">Co możesz zgarnąć na kole fortuny?</div>
-
-          
-        </div>
-      </div>
-    </main>
-  );
-}
 
 export const mockItems = [
   {
@@ -110,5 +70,47 @@ export const mockItems = [
     description: "'Lorem ipsum dolor sit amet",
   },
 ];
+
+function Home() {
+  // const { items /* isError, isLoading */ } = useItemsSet();
+  // const sortedItems = useMemo(
+  //   () => items?.sort((a, b) => b.probability - a.probability),
+  //   [items],
+  // );
+  // console.log(sortedItems);
+
+  return (
+    <main>
+      {/* <Logo src="/LogoCasiBLACK.svg" alt="Lucky Sushi Logo" />
+      <CaseOpener items={sortedItems || []} />
+      <SushiLink href="https://neptunessushi.com/" target="_blank">
+        Order more sushi
+      </SushiLink>
+      {sortedItems && <ListItems items={sortedItems} />}
+      <InfoButton>
+        <InfoModal />
+      </InfoButton> */}
+      <Header />
+
+      <div className="main_container">
+        <div className="main_description">
+          <div className="main_description_first">Zakręć kołem i wygraj sushi!</div>
+          <div className="main_description_second">Wpisz swój kod promocyjny, zakręć ruletką i zgarnij smakowite nagrody od naszej restauracji!</div>
+        </div>
+
+        <Roulette />
+
+        <div className="itemsList">
+          <div className="itemsList_heading">Co możesz zgarnąć na kole fortuny?</div>
+          <div className="itemsList_list">
+            {mockItems.map((item: any, index: number)=> (
+              <Item key={index}/>
+            ))}
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+}
 
 export default Home;
