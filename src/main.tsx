eeb86e7e74339432d518a5fc070c4772fa9682bb
ivankalituk <63204841+ivankalituk@users.noreply.github.com>
2025-06-09 20:12:25 +0300

    new fix

--- src/main.tsx
+++ src/main.tsx
@@ -3,16 +3,12 @@ import { createRoot } from "react-dom/client";
 
 import "./index.scss";
 import App from "./App.tsx";
-import { ThemeProvider } from "styled-components";
-import { theme } from "./theme/themes.ts";
 import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ThemeProvider theme={theme}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
-    </ThemeProvider>
   </StrictMode>
 );
